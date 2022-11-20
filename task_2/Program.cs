24525a97dcef44dcec6e879cc29fc254221eef39
Taras <taras.peschenko.90@mail.ru>
2022-11-20 04:30:01 +0300

    Задача 13

---
+++ task_2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+//  Напишите программу, которая с помощью деления выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+Console.Write("Введите люболе число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+string str = "";
+int result = 0;
+if (number / 100 > 0)
+{
+    str = number.ToString();
+    Console.Write("Третья цифра введённого числа ");
+    Console.WriteLine(str[2]);
+}
+else
+{
+    Console.Write("Третьей цифры нет");
+}
+    
+
+
