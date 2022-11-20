c2c51902cf585800cb93ddf8f4038817bd677b66
Taras <taras.peschenko.90@mail.ru>
2022-11-20 03:21:31 +0300

    Задача 10

---
+++ task_1/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+//  Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+Console.Write("Введите трёхзначное число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+int result = number / 10 % 10;
+
+Console.WriteLine(result);
+
