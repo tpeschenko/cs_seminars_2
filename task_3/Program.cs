705314ca18ae1a83ee06e186fb07e7a414b04452
Taras <taras.peschenko.90@mail.ru>
2022-11-20 04:56:24 +0300

    Задача 15

---
+++ task_3/Program.cs
@@ -0,0 +1,15 @@
+﻿// Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+Console.WriteLine("Программа показывает случайный день недели при каждом запуске!");
+
+Random rand = new Random();
+int number = rand.Next(1, 8);
+
+if ((number == 6) || (number == 7))
+{
+    Console.WriteLine($"Чило равно: {number}");
+    Console.WriteLine("Выходной");
+}
+else
+{   Console.WriteLine($"Чило равно: {number}");
+    Console.WriteLine("Ура, рабочий день!");
+}
